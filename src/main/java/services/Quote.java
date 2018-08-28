326df645fc89f989502bbb50a0541d17d54f39ec
nilstes <nils.tesdal@gmail.com>
2018-08-28 07:02:22 +0200

    Fikset feil

--- src/main/java/services/Quote.java
+++ src/main/java/services/Quote.java
@@ -34,24 +34,24 @@ public class Quote {
     private static List<Quote> quotes = new ArrayList<Quote>() {{
         add(new Quote("Gone With the Wind", "Frankly, my dear, I don't give a damn."));
         add(new Quote("The Godfather", "I'm going to make him an offer he can't refuse."));
-        add(new Quote("Toto, I've got a feeling we're not in Kansas anymore.", "The Wizard of Oz"));
-        add(new Quote("Go ahead, make my day.", "Sudden Impact"));
-        add(new Quote("May the Force be with you.", "Star Wars"));
-        add(new Quote("You talking to me?", "Taxi Driver,"));
-        add(new Quote("I love the smell of napalm in the morning.", "Apocalypse Now"));
-        add(new Quote("E.T. phone home.", "E.T. The Extra-Terrestrial"));
-        add(new Quote("Rosebud.", "Citizen Kane"));
-        add(new Quote("I'll have what she's having.", "When Harry Met Sally,"));
-        add(new Quote("I'll be back.", "The Terminator"));
-        add(new Quote("Mama always said life was like a box of chocolates. You never know what you're gonna get.", "Forrest Gump"));
-        add(new Quote("You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk?", "Dirty Harry"));
-        add(new Quote("Keep your friends close, but your enemies closer.", "The Godfather II"));
-        add(new Quote("Elementary, my dear Watson.", "The Adventures of Sherlock Holmes"));
-        add(new Quote("Here's Johnny!", "The Shining"));
-        add(new Quote("Hasta la vista, baby.", "Terminator 2: Judgment Day"));
-        add(new Quote("Nobody puts Baby in a corner.", "Dirty Dancing"));
-        add(new Quote("I'm king of the world!", "Titanic"));
-        add(new Quote("Yippee-ki-yay, motherfucker.", "Die Hard"));
+        add(new Quote("The Wizard of Oz", "Toto, I've got a feeling we're not in Kansas anymore."));
+        add(new Quote("Sudden Impact", "Go ahead, make my day."));
+        add(new Quote("Star Wars", "May the Force be with you."));
+        add(new Quote("Taxi Driver", "You talking to me?"));
+        add(new Quote("Apocalypse Now", "I love the smell of napalm in the morning."));
+        add(new Quote("E.T. The Extra-Terrestrial", "E.T. phone home."));
+        add(new Quote("Citizen Kane", "Rosebud."));
+        add(new Quote("When Harry Met Sally", "I'll have what she's having."));
+        add(new Quote("The Terminator", "I'll be back."));
+        add(new Quote("Forrest Gump", "Mama always said life was like a box of chocolates. You never know what you're gonna get."));
+        add(new Quote("Dirty Harry", "You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk?"));
+        add(new Quote("The Godfather II", "Keep your friends close, but your enemies closer."));
+        add(new Quote("The Adventures of Sherlock Holmes", "Elementary, my dear Watson."));
+        add(new Quote("The Shining", "Here's Johnny!"));
+        add(new Quote("Terminator 2: Judgment Day", "Hasta la vista, baby."));
+        add(new Quote("Dirty Dancing", "Nobody puts Baby in a corner."));
+        add(new Quote("Titanic", "I'm king of the world!"));
+        add(new Quote("Die Hard", "Yippee-ki-yay, motherfucker."));
     }};
 
     public static Quote getRandom() {
